a4782a343ebd885e14718cad9134495d2467adea
Saul Graves <graves.saul@gmail.com>
2021-05-26 14:28:45 -0400

    Projects page

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,11 +2,16 @@
 
 All notable changes to this project will be documented in this file.
 
-## Unreleased: feature3/projects-page <> YYYY-MM-DD
+## Unreleased:
+
+---
+
+## feature3/projects-page <> 2021-05-26
 
 ### Added
 
----
+-   Projects & ProjectItem components
+-   Project page setup and routing
 
 ## feature2/contact-page <> 2021-05-26
 
---
+++ src/components/projects/ProjectItem.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const ProjectItem = ({
+    name,
+    icon,
+    projectLink,
+    githubLink,
+    description,
+    imgUrl
+}) => {
+    const divStyle = {
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${imgUrl})`,
+        backgroundSize: `120%`,
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
+    };
+    return (
+        <a
+            href={projectLink}
+            target='_blank'
+            rel='noopener noreferrer'
+            className='project-item'
+            style={divStyle}
+        >
+            <div className='project-item-content'>
+                <p>{name}</p>
+                <FontAwesomeIcon icon={icon} className='my' />
+                <p className='project-description'>{description}</p>
+                <a href={githubLink} className='btn my-1'>
+                    View the code
+                </a>
+            </div>
+        </a>
+    );
+};
+
+export default ProjectItem;
