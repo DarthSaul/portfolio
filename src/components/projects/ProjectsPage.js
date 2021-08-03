4f5d79df387642929e6a7c7719a2ac986ab80212
Saul Graves <graves.saul@gmail.com>
2021-08-03 17:58:38 -0400

    Animate on scroll

--- src/components/projects/ProjectsPage.js
+++ src/components/projects/ProjectsPage.js
@@ -5,6 +5,9 @@ import { NavContext } from '../../contexts/NavContext';
 import ProjectItem from './ProjectItem';
 import projects from '../../utilities/projects';
 
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 const ProjectsPage = () => {
     const { setActivePage } = useContext(NavContext);
 
@@ -12,10 +15,16 @@ const ProjectsPage = () => {
         setActivePage('Projects');
     }, [setActivePage]);
 
+    AOS.init();
+
     return (
         <>
             <div className='about-wrapper py-4'>
-                <div className='container row justify-content-evenly my-5'>
+                <div
+                    className='container row justify-content-evenly my-5'
+                    data-aos='fade-right'
+                    data-aos-duration='1500'
+                >
                     {projects.map((item, ind) => (
                         <div
                             className='col-10 col-md-7 col-lg-5 mb-5 mx-3'
