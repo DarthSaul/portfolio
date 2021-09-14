b49fcf2517cffec83ef63eb3753b487a566bf744
Saul Graves <graves.saul@gmail.com>
2021-09-14 14:40:01 -0400

    Link resume

--- src/utilities/contactInfo.js
+++ src/utilities/contactInfo.js
@@ -1,28 +1,28 @@
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faFile } from '@fortawesome/free-solid-svg-icons';
 
-import gmail from '../img/gmail.jpg';
-import linkedIn from '../img/linkedIn.jpg';
-import github from '../img/github.jpg';
+import resume from '../Saul_Graves-Resume.pdf';
 
 const contactItems = [
     {
         name: 'GitHub',
         link: 'https://github.com/DarthSaul',
-        icon: faGithub,
-        backgroundImg: github
+        icon: faGithub
     },
     {
         name: 'Email',
         link: 'mailto:graves.saul@gmail.com',
-        icon: faEnvelope,
-        backgroundImg: gmail
+        icon: faEnvelope
     },
     {
         name: 'LinkedIn',
         link: 'https://www.linkedin.com/in/saulgraves/',
-        icon: faLinkedin,
-        backgroundImg: linkedIn
+        icon: faLinkedin
+    },
+    {
+        name: 'Resume',
+        link: resume,
+        icon: faFile
     }
 ];
 
