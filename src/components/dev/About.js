ff0b698613e6371f8ddef572a55ecc650f654f51
Saul Graves <graves.saul@gmail.com>
2022-11-20 22:29:44 -0500

    update nav, name at page top

--- src/components/dev/About.js
+++ src/components/dev/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 import contactInfo from '../../utilities/contactInfo';
 
@@ -9,52 +9,92 @@ const About = () => {
 	return (
 		<div className="container-fluid">
 			<div className="row justify-content-between">
-				<div xs={12} lg={6} className="col-xs-12 col-lg-6 columns">
-					<div className="mb-4 h2">Bio</div>
+				<div
+					xs={12}
+					lg={6}
+					className="col-xs-12 col-lg-6 columns"
+				>
+					<div className="mt-2 mb-4 h2">Bio</div>
 					<div className="vstack gap-4 h6 fw-light text-muted mb-4">
 						<div>
-							A few years ago, I decided to teach myself
-							full-stack web development because I wanted to
-							create immersive digital experiences that make an
-							immediate impact on users no matter where they come
-							from.
+							A few years ago, I
+							decided to teach myself
+							full-stack web
+							development because I
+							wanted to create
+							immersive digital
+							experiences that make an
+							immediate impact on
+							users no matter where
+							they come from.
 						</div>
 						<div>
-							I obsess over clean, scalable, and well-designed
-							backend architecture. On the frontend, I build
-							beautiful user experiences rooted in responsive
-							UX/UI and modern web design.
+							I obsess over clean,
+							scalable, and
+							well-designed backend
+							architecture. On the
+							frontend, I build
+							beautiful user
+							experiences rooted in
+							responsive UX/UI and
+							modern web design.
 						</div>
 						<div>
-							My JavaScript expertise lies in Node, Express,
+							My JavaScript expertise
+							lies in Node, Express,
 							React, and Vue.
 						</div>
 						<div>
-							While I'm not coding, I'm reading fantasy books,
-							cooking delicious food (I think), and brewing A LOT
-							of coffee.
+							While I'm not coding,
+							I'm reading fantasy
+							books, cooking delicious
+							food (I think), and
+							brewing A LOT of coffee.
 						</div>
 					</div>
 					<div className="h4">Technologies</div>
 					<div className="row mb-3 fw-light text-muted">
 						<div className="col-auto me-3">
 							<div className="vstack">
-								<div>JavaScript</div>
-								<div>Node, Express</div>
-								<div>React, Vue</div>
+								<div>
+									JavaScript
+								</div>
+								<div>
+									Node,
+									Express
+								</div>
+								<div>
+									React,
+									Vue
+								</div>
 							</div>
 						</div>
 						<div className="col-auto me-3">
 							<div className="vstack">
-								<div>SQL (PostgreSQL)</div>
-								<div>NoSQL (MongoDB)</div>
-								<div>Git, GitHub, AWS</div>
+								<div>
+									SQL
+									(PostgreSQL)
+								</div>
+								<div>
+									NoSQL
+									(MongoDB)
+								</div>
+								<div>
+									Git,
+									GitHub,
+									AWS
+								</div>
 							</div>
 						</div>
 						<div className="col-auto">
 							<div className="vstack">
-								<div>HTML, CSS</div>
-								<div>Bootstrap</div>
+								<div>
+									HTML,
+									CSS
+								</div>
+								<div>
+									Bootstrap
+								</div>
 								<div>Jest</div>
 							</div>
 						</div>
@@ -63,29 +103,51 @@ const About = () => {
 					<div className="row mb-4 fw-light text-muted">
 						<div className="col-auto me-3">
 							<div className="vstack">
-								<div>Responsive web design</div>
+								<div>
+									Responsive
+									web
+									design
+								</div>
 								<div>Figma</div>
 							</div>
 						</div>
 						<div className="col-auto me-3">
 							<div className="vstack">
-								<div>REST APIs</div>
-								<div>Agile, Scrum</div>
+								<div>
+									REST
+									APIs
+								</div>
+								<div>
+									Agile,
+									Scrum
+								</div>
 							</div>
 						</div>
 					</div>
-					<div className="h4">Let's get in touch</div>
+					<div className="h4">
+						Let's get in touch
+					</div>
 					<div className="about-page-links">
-						{contactInfo.map((item, ind) => (
-							<a
-								key={ind}
-								href={item.link}
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								<FontAwesomeIcon icon={item.icon} />
-							</a>
-						))}
+						{contactInfo.map(
+							(item, ind) => (
+								<a
+									key={
+										ind
+									}
+									href={
+										item.link
+									}
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<FontAwesomeIcon
+										icon={
+											item.icon
+										}
+									/>
+								</a>
+							)
+						)}
 					</div>
 				</div>
 
--- src/components/dev/Projects.js
+++ src/components/dev/Projects.js
@@ -6,20 +6,37 @@ import '../../css/Projects.css';
 const Projects = () => {
 	return (
 		<div className="container-fluid projects-wrapper">
+			<div className="row">
+				<div className="col-xs-12 display-3">
+					SAUL GRAVES
+				</div>
+			</div>
 			<div className="row g-4">
 				<div className="col-xs-12 col-lg-7">
 					<div className="row mb-4">
 						<div className="col project-container">
-							<ProjectCard project={projects.conservationLabs} />
+							<ProjectCard
+								project={
+									projects.conservationLabs
+								}
+							/>
 						</div>
 					</div>
 
 					<div className="row g-4">
 						<div className="col-xs-12 col-md-7 project-container projects-2">
-							<ProjectCard project={projects.ghibli} />
+							<ProjectCard
+								project={
+									projects.ghibli
+								}
+							/>
 						</div>
 						<div className="col-xs-12 col-md mb-2 project-container projects-3">
-							<ProjectCard project={projects.marvel} />
+							<ProjectCard
+								project={
+									projects.marvel
+								}
+							/>
 						</div>
 					</div>
 				</div>
@@ -27,12 +44,20 @@ const Projects = () => {
 				<div className="col">
 					<div className="row mb-4">
 						<div className="col project-container projects-food projects-4">
-							<ProjectCard project={projects.foodFromFriends} />
+							<ProjectCard
+								project={
+									projects.foodFromFriends
+								}
+							/>
 						</div>
 					</div>
 					<div className="row">
 						<div className="col project-container projects-coffee projects-5">
-							<ProjectCard project={projects.coffeeCorner} />
+							<ProjectCard
+								project={
+									projects.coffeeCorner
+								}
+							/>
 						</div>
 					</div>
 				</div>
